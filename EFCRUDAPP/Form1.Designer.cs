68f2dce4394bed1fc800c298973d05aa9b60e68c
Taynara Silva <taynarassilva14@gmail.com>
2021-11-03 09:34:52 -0300

    Criando a funcao do botao Cancel

--- EFCRUDAPP/Form1.Designer.cs
+++ EFCRUDAPP/Form1.Designer.cs
@@ -143,6 +143,7 @@ namespace EFCRUDAPP
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // Form1
             // 
--- EFCRUDAPP/Form1.cs
+++ EFCRUDAPP/Form1.cs
@@ -12,6 +12,7 @@ namespace EFCRUDAPP
 {
     public partial class Form1 : Form
     {
+        Customer model = new Customer();
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +20,20 @@ namespace EFCRUDAPP
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Clear();
+        }
 
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            txtFirstName.Text = txtLastName.Text = txtCity.Text = txtCity.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+            model.CustomerID = 0;
         }
     }
 }
